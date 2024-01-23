c88270535632e6e68d914ba155136ceba829b07c
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-01-23 15:52:03 +0800

    Done with pseudocode

---
+++ exercise10.py
@@ -0,0 +1,11 @@
+# Given
+list1 = [10, 20, 25, 30, 35]
+list2 = [40, 45, 60, 75, 90]
+
+# Define a function, with two list as a parameter
+# this function should return a new list
+# loop through the first list to get all the odd numbers
+# loop trough the second list to get all the even numbers
+# return the modified new list
+
+
